eeda7ca008327974da505f4f178d51def8c6b420
Dennis Muasya <kyalodennis1738@gmail.com>
2025-04-01 12:14:21 +0300

    f

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 // Function that returns the first two drivers from an array
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
-};
+const returnFirstTwoDrivers= function (drivers){
+return drivers.slice(0,2);
+}
 
 // Function that returns the last two drivers from an array
 const returnLastTwoDrivers = function(drivers) {
@@ -13,7 +13,7 @@ const returnLastTwoDrivers = function(drivers) {
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 // Function that creates a multiplier function
-// Export functions for testing
+
 
 const createFareMultiplier = (multiplier) => {
     return (fare) => fare * multiplier;
@@ -23,14 +23,6 @@ const fareTripler = createFareMultiplier(3);
 const selectDifferentDrivers = (drivers, selectFunction) => {
     return selectFunction(drivers);
 };
-module.exports = {
-    returnFirstTwoDrivers,
-    returnLastTwoDrivers,
-    selectingDrivers,
-    createFareMultiplier,
-    fareDoubler,
-    fareTripler,
-    selectDifferentDrivers
-};
+
 
 
